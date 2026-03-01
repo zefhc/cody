7ecf7e268fd136d94b2bf5889921d11ac38db936
AMDINTELPRO <amdintelpro@gmail.com>
2026-03-01 18:51:20 +0300

    Add files via upload

--- Урок 16/script.js
+++ Урок 16/script.js
@@ -1,26 +1,26 @@
 "use strict"
 
 const telegram_bot_token = '8254727494:AAFMm5jPC9uUnapJLvHHORRQUiPSrx2VUm4';
-const telegram_chat_id = '-3777803894';
+const telegram_chat_id = '-1003777803894';
 const api = `https://api.telegram.org/bot${telegram_bot_token}/sendMessage`;
 
 async function sendEmailTelegram(event) {
-    event.preventDeafult()
+    event.preventDefault()
 
     const form = event.target;
     const formBtn = document.querySelector('.form_button')
-    const formSendResult = document.querySelector()
+    const formSendResult = document.querySelector('.form_send_result')
     formSendResult.textContent = '';
 
-    const  {name, phone, problem} = Object.fromEntries(new FormData(from).entries)
-    const text = `Заявка от ${name}! /n Телефон: ${phone} /n Краткое описание проблемы: ${problem}`
+    const  {name, phone, problem} = Object.fromEntries(new FormData(form).entries())
+    const text = `Заявка от ${name}!\nТелефон: ${phone}\nКраткое описание проблемы: ${problem}`
 
     try  {
         formBtn.textContent = 'Отправка...'
         const respone = await fetch(api, {
             method: 'POST',
-            header:{
-                'Content-Type' : 'application/jsons'
+            headers:{
+                'Content-Type' : 'application/json'
             },
             body: JSON.stringify({
                 chat_id: telegram_chat_id,
@@ -28,20 +28,20 @@ async function sendEmailTelegram(event) {
             })
         });
 
-        if (Response.ok){
+        if (respone.ok){
             formSendResult.textContent = 'Спасибо за ашу заявку, мы связжемся с вами в ближайшее время.'
             form.reset()
         }
         else{
-            throw new Error(Response.statusText)
+            throw new Error(respone.statusText)
         }
     }
     catch (error){
         console.error(error)
-        formSendResult.textContent = 'Анкета не отправлена! Попрбуйте позжею.'
+        formSendResult.textContent = 'Анкета не отправлена! Попробуйте позже.'
         formSendResult.style.color = 'red'
     }
     finally{
-        formBtn.textContent = 'Отправитьы'
+        formBtn.textContent = 'Отправить'
     }    
 }
